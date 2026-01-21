44dfc8d70ee03e3877919644acdac0e9dd66c3a2
irydmy13 <kotiukir@gmail.com>
2026-01-21 11:14:19 +0200

    Define domain entities and service contracts

---
+++ Car.Core/ServiceInterface/ICarServices.cs
@@ -0,0 +1,16 @@
+﻿using Car.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Car.Core.ServiceInterface
+{
+    public interface ICarServices
+    {
+        Task<IEnumerable<Domain.Car>> GetAll();
+        Task<Domain.Car> Get(Guid id);
+        Task<Domain.Car> Create(Domain.Car car);
+        Task<Domain.Car> Update(Domain.Car car);
+        Task<Domain.Car> Delete(Guid id);
+    }
+}
