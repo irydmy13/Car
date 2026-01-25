97c7bbd33841ba3b7517fd4c259475418b6a4edf
irydmy13 <kotiukir@gmail.com>
2026-01-25 23:29:26 +0200

    add dto

---
+++ Car.Core/Dto/CarDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car.Core.Dto
+{
+    internal class CarDto
+    {
+    }
+}
